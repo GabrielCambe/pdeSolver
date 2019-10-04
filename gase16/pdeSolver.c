b1137d40ed9a69a8053a81c00801567e3dee0a82
GABRIEL DE ALMEIDA SALES EVARISTO <gase16@urquell>
2019-10-04 17:18:55 -0300

    avancei a implementação da alocação do sistema depois de definir uma representação para o problema

--- gase16/pdeSolver.c
+++ gase16/pdeSolver.c
@@ -21,14 +21,14 @@
   A chamada deve ser da forma:
 pdeSolver -nx <Nx> -ny <Ny> -i <maxIter> -o arquivo_saida
 
--nx: parâmetro obrigatório definindo o número de pontos a serem calculados na dimensão X.
+  -nx: parâmetro obrigatório definindo o número de pontos a serem calculados na dimensão X.
 
--ny: parâmetro obrigatório definindo o número de pontos a serem calculados na dimensão Y.
+  -ny: parâmetro obrigatório definindo o número de pontos a serem calculados na dimensão Y.
 
--i maxIter: parâmetro obrigatório definindo o número de iterações a serem executadas.
+  -i maxIter: parâmetro obrigatório definindo o número de iterações a serem executadas.
 
--o arquivo_saida: parâmetro no qual arquivo_saida é o caminho completo para o arquivo que vai conter a solução (valores da função em cada ponto da grade). Caso este parâmetro não esteja especificado, a saída deve ser stdout
-Esta solução deve estar formatada para servir de entrada ao comando gnuplot, de forma que ele possa automaticamente gerar o gráfico da função.
+  -o arquivo_saida: parâmetro no qual arquivo_saida é o caminho completo para o arquivo que vai conter a solução (valores da função em cada ponto da grade). Caso este parâmetro não esteja especificado, a saída deve ser stdout
+  Esta solução deve estar formatada para servir de entrada ao comando gnuplot, de forma que ele possa automaticamente gerar o gráfico da função.
 
 Além disso, no início do arquivo, deve constar sob a forma de comentários do gnuplot:
   1. O tempo médio de execução de cada iteração do Método de Gauss-Seidel.
@@ -37,24 +37,31 @@ Além disso, no início do arquivo, deve constar sob a forma de comentários do 
 
   2. O valor do resíduo para cada iteração.
 
-###########
-# Tempo Método GS: <média de tempo para o cálculo de uma iteração do método, em milisegundos>
-#
-# Norma L2 do Residuo
-# i=1: <valor>
-# i=2: <valor>
-# i=3: <valor>
-# ...
-##########
+  ###########
+  # Tempo Método GS: <média de tempo para o cálculo de uma iteração do método, em milisegundos>
+  #
+  # Norma L2 do Residuo
+  # i=1: <valor>
+  # i=2: <valor>
+  # i=3: <valor>
+  # ...
+  ##########
 */
+
+#define myMalloc(tam, tipo) (tipo*) malloc(tam*sizeof(tipo))
+#define testMalloc(pointer) if( pointer == NULL ) abort()
+#define index(i,j,n) ((i*n)+j)
 
 typedef double Real_t;
 
-typedef struct {
 
+typedef struct {
+  Real_t dInfx, dInfy, dPrin, dSupx, dSupy;
+  Real_t *b;
+  unsigned int nx;
+  unsigned int ny;
 }Sist_Lin;
 
-typedef Solucao;
 
 // Trata a entrada passada pela linha de comando
 /* PRECISA MUDAR A CHECAGEM DA VALIDADE DOS ARGUMENTOS -nx, -ny e -i PARA C CERTIFICAR QUE NÃO EXISTEM LETRAS NO NUMERO PASSADO */
@@ -128,17 +135,60 @@ void le_comandos(int argc, char* argv[], unsigned int* nx, unsigned int* ny, uns
   return;
 }
 
-Solucao* aloca_e_inicializa_solucao( Real_t var_init, FUNCAO f1, FUNCAO f2, FUNCAO f3, FUNCAO F4 ){
-  aloca_solucao();
-  // inicializa solucao com 0s nos pontos interiores e com cada uma das funções nas bordas
-  return NULL;
+Real_t* aloca_e_inicializa_solucao( Sist_Lin * sist ){
+  // inicializa a solução com 0s nos pontos interiores e com cada uma das funções nas bordas (preencha o vetor com zero e as condições de contorno)
+  Real_t* solucao = myMalloc(((sist->nx+2)*(sist->ny+2)), Real_t);
+  testMalloc( solucao );
+
+  for(unsigned int i = 0; i < ((sist->nx)+2); ++i){
+    for(unsigned int j = 0; i < ((sist->ny)+2); ++i){
+      // acessa o vetor x
+      solucao[ index(i,j, ((sist->nx+2)*(sist->ny+2))) ];
+    }
+  }
+
+  return solucao;
 }
 
-Sist_Lin* aloca_sist(){
-  return NULL;
+// alocar as estruturas necessárias pra resolver o problema:
+Sist_Lin* aloca_sist(unsigned int nx, unsigned int ny){
+
+  Sist_Lin* sist = myMalloc( 1, Sist_Lin );
+  testMalloc( sist );
+
+  // alocar e inicializar vetor b
+    // calcular o valor dos passos
+  Real_t hx = 1/(nx+1); Real_t hy = 1/(ny+1);
+ 
+    // criar vetor b de tamanho (nx+2)(ny+2)
+  sist->b = myMalloc( (nx+2)*(ny+2), Real_t );
+  testMalloc( sist->b );
+  
+    // b depende de vetores x e y, de tamanho nx+2 e ny+2, qu possuem os valores dos passos dados em x e em y
+    // populados com x = 0; x <= PI; x += PI/(nx+1) e y = 0; y <= PI; y += PI/(ny+1)
+    // preencher esse vetor com os valores de f(x,y) = 4*PI*PI*( sin( 2*PI*x ) * sinh( PI*y ) + sin( 2*PI*(PI-x) ) * sinh( PI*(PI - y) )
+  Real_t x = 0;
+  Real_t y = 0;
+  for(unsigned int i = 0; i < (nx+2); ++i, x += hx){
+    y = 0;
+    for(unsigned int j = 0; j < (ny+2); ++j, y += hy){
+      sist->b[ index(i,j,((sist->ny)+2)) ] = 4*PI*PI*( sin( 2*PI*x ) * sinh( PI*y ) + sin( 2*PI*(PI-x) ) * sinh( PI*(PI - y) ));
+    }
+  }
+  sist->nx = nx;
+  sist->ny = ny;
+
+    // criar os valores que representam a matriz A
+  sist->dInfy = -((1/(hx*hx)) + (1/(2*hx))); 
+  sist->dInfx = -((1/(hy*hy)) + (1/(2*hy)));
+  sist->dPrin = ((4*PI*PI)+(2/(hx*hx))+(2/(hy*hy)));
+  sist->dSupy = -((1/(hy*hy)) - (1/(2*hy)));
+  sist->dSupx = -((1/(hx*hx)) - (1/(2*hx)));
+
+  return sist;
 }
 
-void escreve_solucao_gnuplot(char* arq_saida, Real_t tempo_medio, unsigned int num_iter, Real_t* residuo_iter){
+void escreve_solucao_gnuplot(char* arq_saida, Real_t tempo_medio, unsigned int num_iter, Real_t* residuo_iter, Sist_Lin* sist){
   srand(time(NULL));
   if( arq_saida != NULL ){
     // abra o arquivo de saida
@@ -153,8 +203,11 @@ void escreve_solucao_gnuplot(char* arq_saida, Real_t tempo_medio, unsigned int n
     fprintf(saida, "###########\n");
   
     // escreva os valores de x, y e u(x,y) no arquivo de saida
-    for(unsigned int i = 0; i < 10; ++i){
-      fprintf(saida, "%lf\t%lf\t%lf\n", (Real_t)rand(), (Real_t)rand(), (Real_t)rand());
+    for(unsigned int i = 0; i < ((sist->nx)+2); ++i){
+      for(unsigned int j = 0; i < ((sist->ny)+2); ++i){
+        // acessa o vetor x
+         fprintf(saida, "%lf\t%lf\t%lf\n", (Real_t)rand(), (Real_t)rand(), (Real_t)rand());
+      }
     }
     // feche o arquivo de saída
     fclose(saida);
@@ -170,14 +223,21 @@ void escreve_solucao_gnuplot(char* arq_saida, Real_t tempo_medio, unsigned int n
     printf("###########\n");
   
     // escreva os valores de x, y e u(x,y) no arquivo de saida
-    for(unsigned int i = 0; i < 10; ++i){
-      printf("%lf\t%lf\t%lf\n", (Real_t)rand(), (Real_t)rand(), (Real_t)rand());
+    for(unsigned int i = 0; i < ((sist->nx)+2); ++i){
+      for(unsigned int j = 0; i < ((sist->ny)+2); ++i){
+        printf("%lf\t%lf\t%lf\n", (Real_t)rand(), (Real_t)rand(), (Real_t)rand());
+      }
     }
   }
   return;
 }
 
-void libera_sist(){
+void libera_sist(Sist_Lin* sist){
+    // desalocar o vetor b
+  free(sist->b);
+    // desalocar o sistema 
+  free(sist);
+  
   return;
 }
 
@@ -191,32 +251,30 @@ int main(int argc, char* argv[]){
 
   // ler entradas da linha de comando
   le_comandos( argc, argv, &nx, &ny, &iter, &arq_saida );
-  // alocar as estruturas necessárias pra resolver o problema:
-    // calcular o valor dos passos
-    // alocar e inicializar vetor b
-      // teremos vetores x e y, de tamanho nx+2 e ny+2
-      // populados com x = 0; x <= PI; x += PI/(nx+1) e y = 0; y <= PI; y += PI/(ny+1) 
-    // alocar e inicializar sistema de equações 
-    // E preencha o vetor com as condições de contorno
-  Sist_Lin *sistema = aloca_sist();
-    // alocar e inicializar vetor de residuos
+
+  // alocar e inicializar sistema de equações 
+  Sist_Lin *sistema = aloca_sist(ny, ny);
+  // alocar e inicializar o vetor solução nulo
+  Real_t *x = aloca_e_inicializa_solucao( sistema );
+  // alocar e inicializar vetor de residuos
   Real_t norma_residuo_por_iter[5] = { 4.0, 4.58, 13.57, 19.12, 5.55 };
-    // alocar e inicializar o vetor solução nulo
-  Solucao *x = aloca_e_inicializa_solucao( 0, f1, f2, f3, f4 );
   
   // comece um for ate o numero maximo de iteracoes
-  for(unsigned int k = 1; k <= iter; ++k )
+  unsigned int k;
+  for( k = 1; k <= iter; ++k ){
+    if(rand() % 23) break;
+  }
     // resolva a equacao diferencial por diferencas finitas e gaus-siedel
     // inicializa x e y com PI/(nx+1) e PI/(ny+1)
     // percorra os pontos atualizando os valores de
     // calcule o residuo para esta iteração
   
   // escreve o arquivo de saída
-  escreve_solucao_gnuplot( arq_saida, 7.98, k, norma_residuo_por_iter );
+  escreve_solucao_gnuplot( arq_saida, 7.98, k, norma_residuo_por_iter, sistema );
   
   // libere a memoria usada para as estruturas
-  libera_sist();
-  libera_solucao();
+  libera_sist( sistema );
+  libera_solucao( x );
   //free(norma_residuo);
   
   return 0;
